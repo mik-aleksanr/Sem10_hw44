58d3c6e227567e255fed8466a0d8942fa5f52c31
AlexandrMihelin <mik-aleksanr@yandex.ru>
2023-12-13 19:04:26 +0300

    Sem10hw44

--- hw44.py
+++ hw44.py
@@ -12,9 +12,18 @@ data.head()
 
 import random
 import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+
 
 lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI': lst})
 data.head()
+
+coder = OneHotEncoder(handle_unknown='ignore')  # создаем кодировщик
+cod_df = pd.DataFrame(coder.fit_transform(data[['whoAmI']]).toarray())  # выполняем горячее кодирование
+# для столбца
+print(data.join(cod_df))  # это для наглядности первоначальный столбец и кодированный
+cod_df.columns = ['human', 'robot']  # добавляем название столбцов по названиям категориальных переменных
+print(cod_df)  # результат
